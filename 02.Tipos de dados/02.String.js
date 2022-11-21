d4860c500a20308af9ef7ec478193a99b42f9ba5
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-21 15:28:32 -0300

    String

---
+++ 02.Tipos de dados/02.String.js
@@ -0,0 +1,12 @@
+/*
+    String
+
+    - Cadeia de caracteres 
+
+    ""// aspas duplas
+    ''// aspas simples
+    ``// template literais ou template string
+*/
+
+console.log(`miguel santos ${2 + 2}`)
+console.log(2 + 2)
