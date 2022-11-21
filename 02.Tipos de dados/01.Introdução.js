fdda5bb7a00f9a758c8365b4d33b22f22a286999
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-21 15:28:23 -0300

    Introdução

---
+++ 02.Tipos de dados/01.Introdução.js
@@ -0,0 +1,15 @@
+/*
+    # Tipos de dados 
+
+    - Gramática 
+        * Elementos da linguagem e suas combinações 
+        * A arte de falar e escrever corretamente
+
+    - vocabulário
+        * Conjunto de termos e expressões
+        * Agrupamento de palavras
+
+    * Precisamos saber como escrever 
+    * Precisamos saber os signicados
+    * Precisamos continuar aprendendo, para crescer nosso vocabulário
+*/
