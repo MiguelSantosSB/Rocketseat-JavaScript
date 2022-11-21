f9a558389aa505fd38b73cf315025877c4794bc8
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-21 15:30:02 -0300

    Tipo de dados

---
+++ 02.Tipos de dados/08.Tipos de dados.js
@@ -0,0 +1,33 @@
+/*
+    # Tipos de dados
+
+        Conforme o ECMAScript standard temos 9 tipos de dados:
+    
+    * Data types
+            * Primitive / Primitive values
+            * Structural
+            * Structural Primitive
+
+    ## Primitive
+
+    - String
+    - Number
+    - Boolean
+    - undefined
+    - Symbol
+    - BigInt
+
+    ## Structural
+
+    - Object
+        * Array
+        * Map
+        * Set
+        * Date
+        * ...
+    - Function
+
+    ## Primitive Struction / Structural Root Primitive
+
+    - null
+*/
