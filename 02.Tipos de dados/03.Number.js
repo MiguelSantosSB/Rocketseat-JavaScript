b8a398e7c8de2551db84ff72082391af13f2e332
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-21 15:28:39 -0300

    Number

---
+++ 02.Tipos de dados/03.Number.js
@@ -0,0 +1,24 @@
+/*
+    Number
+        - números 
+
+    33 // inteiro 
+    12.5 // reais
+    NaN // Not a Number
+    Infinity // infinito
+*/
+
+console.log(2 + 5)
+
+console.log(2.5 + 2.7)
+
+console.log(2 / "aaaa")
+
+
+
+
+
+
+
+
+
