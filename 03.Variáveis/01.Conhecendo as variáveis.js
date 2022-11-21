54a72885b0803cb4774a48b67e9a4968b326e0f8
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-21 15:36:45 -0300

    Conhecendo as Variáveis

---
+++ 03.Variáveis/01.Conhecendo as variáveis.js
@@ -0,0 +1,22 @@
+/*
+    # Variáveis
+
+        - Nomes simbólicos para receber algum valor
+        - Atalho de código
+        - Identificadores
+    * 3 palavras reservadas para variável
+        - var 
+        - let
+        - const
+*/
+
+var clima = "Quente"
+
+console.log(clima)
+
+clima = "Frio"
+
+console.log(clima)
+
+const dia = 21
+
