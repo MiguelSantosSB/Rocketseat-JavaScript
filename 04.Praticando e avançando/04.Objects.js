d3d8332a6b1d197e9087467de7979e6d58d6382d
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-23 15:02:25 -0300

    Objects

---
+++ 04.Praticando e avançando/04.Objects.js
@@ -0,0 +1,13 @@
+const person = {
+    name: 'John',
+    age: 30,
+    wight: 88.6,
+    isAdim: true
+}
+
+console.log(person.name)
+console.log(person.age)
+console.log(person.wight)
+console.log(person.isAdim)
+
+console.log(`${person.name} tem ${person.age} anos e tem ${person.wight}kg.`)
