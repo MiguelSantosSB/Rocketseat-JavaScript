58433a0f576314c64cf5f265ccb1ec780458e2f8
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-23 15:01:57 -0300

    Agrupando Declarações

---
+++ 04.Praticando e avançando/02.Agrupando declarações.js
@@ -0,0 +1,18 @@
+
+var name = 'miguel'
+
+// tipo de dado colocado na variável
+console.log(typeof name)
+
+// agrupamento de declarações
+let age, isHuman
+/*
+a mesma coisa de:
+let age
+let isHuman
+*/
+
+age = 18
+isHuman = true
+
+console.log(`${name} ${age} ${isHuman}`)
