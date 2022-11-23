f5dcb790a2fea646ed576d4891b1a4251f96b6e8
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-23 15:02:30 -0300

    Arrays

---
+++ 04.Praticando e avançando/05.Arrays.js
@@ -0,0 +1,15 @@
+const animals = [
+    'lION',
+    'MONKEY',
+    {
+    name: 'CAT',
+    age: 3    
+    }
+]
+
+console.log(animals[0])
+console.log(animals[1])
+console.log(animals[3])
+
+
+console.log(animals[2].age)
