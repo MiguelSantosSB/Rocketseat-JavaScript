b3fd408c63f96144375201a464b97b50092521cd
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-23 15:02:17 -0300

    Concatenando e Interpolando Variáveis

---
+++ 04.Praticando e avançando/03.Concatenando e interpolando variáveis.js
@@ -0,0 +1,22 @@
+
+var name = 'miguel'
+
+// tipo de dado colocado na variável
+console.log(typeof name)
+
+// agrupamento de declarações
+let age, isHuman
+/*
+a mesma coisa de:
+let age
+let isHuman
+*/
+
+age = 18
+isHuman = true
+
+// console.log(`${name} ${age} ${isHuman}`)
+
+console.log('O '+ name +' tem ' + age +' anos.' )
+
+console.log(`O ${name} tem ${age} anos.`)
