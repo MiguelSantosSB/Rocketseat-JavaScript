bf377c95e009ae120d98d3583cbb435c88ce875a
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-23 15:02:38 -0300

    Exercícios

---
+++ 04.Praticando e avançando/06.Exercícios.js
@@ -0,0 +1,39 @@
+//1
+
+let weight
+
+//2
+
+console.log(typeof weight)
+
+//3
+
+let name, age, stars, isSubscribed
+    name = 'john'
+    age = 26
+    stars = 23.5
+    isSubscribed = true
+
+//4
+
+var studant = {}
+
+console.log(typeof studant)
+
+//4.2
+
+var studant = {
+    name: 'john',
+    age: 22,
+    weight: 82.4
+}
+
+// console.log(`${studant.name} tem  ${studant.age} de idade e pesa ${studant.weight} kg.`)
+
+//5
+
+const students = []
+
+// console.log(typeof students)
+
+//6
