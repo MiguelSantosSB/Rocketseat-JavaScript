f7fe44de42111c5277af684a483aa825e95dcfe5
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-26 16:01:11 -0300

    Retornando Valores

---
+++ 05.Funções/03.Retornando valores.js
@@ -0,0 +1,13 @@
+// Retornando valores
+
+const sum1 = function(number1, number2){
+    total = number1 + number2 
+    return total
+}
+
+let number1 = 24
+let number2 = 35
+
+console.log(`O número 1 e: ${number1}`)
+console.log(`O número 2 e: ${number2}`)
+console.log(`A soma desses números e: ${sum1(number1, number2)}`)
