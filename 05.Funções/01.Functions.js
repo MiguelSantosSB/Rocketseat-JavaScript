a6138db3b33bdb5f392cc93b296e7fcdf9d7625c
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-26 16:00:36 -0300

    Functions

---
+++ 05.Funções/01.Functions.js
@@ -0,0 +1,11 @@
+function CriarFrase(){
+    console.log("Frases para mostrar como funciona a função no js")
+    console.log("Frases para mostrar como funciona a função no js")
+    console.log("Frases para mostrar como funciona a função no js")
+}
+
+// Chamando a função 
+
+CriarFrase()
+
+
