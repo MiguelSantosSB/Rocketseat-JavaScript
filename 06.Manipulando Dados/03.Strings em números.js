fa4d2375fbe082ed89bc9e3933e42618974c8260
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-27 12:59:11 -0300

    String em Números

---
+++ 06.Manipulando Dados/03.Strings em números.js
@@ -0,0 +1,11 @@
+/*
+    Manipulando String e Números
+
+    Transforma String em Número e Número em String
+*/ 
+
+let string = '123'
+console.log(Number(string) + 0)
+
+let number = 321
+console.log(String(number) + 0)
