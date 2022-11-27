849f6183b969f399f466824cad74e260ac513107
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-27 12:58:45 -0300

    Type Conversion Coersion

---
+++ 06.Manipulando Dados/02.Type conversion coersion.js
@@ -0,0 +1,10 @@
+/*]
+    Type conversion (typecasting) vs Type coersion
+
+    * Alteração de um tipo de dado para outro tipo
+*/ 
+
+console.log('9' + 5)
+// nesse caso a linguagem de forma forçada irá transformar o "5" em string
+
+console.log(Number('9') + 5)
