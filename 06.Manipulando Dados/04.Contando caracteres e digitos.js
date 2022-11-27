5a44e4e66bf270c8ec2f82f734b14d8663a65baa
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-27 13:00:04 -0300

    Contando Caracteres e Dígitos

---
+++ 06.Manipulando Dados/04.Contando caracteres e digitos.js
@@ -0,0 +1,10 @@
+/*
+    Contar quantos caracteres tem uma palavra e quantos 
+    digitos tem um número 
+*/ 
+
+let string = 'umacasamiutoengraçada'
+console.log(string.length)
+
+let number = 12334
+console.log(String(number).length)
