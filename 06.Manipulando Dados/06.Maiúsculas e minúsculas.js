c9318cf8d82618ad5f2b4e2878a799a78a0eb0c6
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-27 13:00:42 -0300

    Maiúsculas e Minúsculas

---
+++ 06.Manipulando Dados/06.Maiúsculas e minúsculas.js
@@ -0,0 +1,11 @@
+/*
+    Transforma letras minúsculas em maiúsculas
+*/ 
+
+let frase = 'Programar é muito legal!'
+
+console.log(frase.toLowerCase())
+//  toLowerCase - tudo minúsculo
+
+console.log(frase.toLocaleUpperCase())
+//  toLocaleUpperCase - tudo maiúsculo
