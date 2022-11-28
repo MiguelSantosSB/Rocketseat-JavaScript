02d9e53ee7149e3768da55564f756d94c80353e5
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-28 19:48:11 -0300

    Operadores Lógicos

---
+++ 07.Expressões e Operadores/09.Operadores lógicos.js
@@ -0,0 +1,21 @@
+/*
+    Operadores lógicos
+
+    2 valores booleanos, quando verificados, resultará
+    em verdadeiro ou falso
+*/ 
+
+let presunto = true
+let queijo = false
+
+//  AND/E                   &&
+console.log(presunto && queijo)
+// os dois tem que estar true para ele ser true 
+
+// OR/OU                    ||
+console.log(presunto || queijo)
+// precisa apenas que um seja true para ele tbm ser 
+
+// NOT                       !
+console.log(!presunto)
+//  se algo e true ele transforma para false e assim ao contrario tbm
