52bc9bd940d55fafedcb5b8e136d70c978009287
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-28 19:47:26 -0300

    Operadores Aritméticos

---
+++ 07.Expressões e Operadores/03.Operadores aritméticos.js
@@ -0,0 +1,22 @@
+/*
+    Operadores Aritméticos
+*/   
+    // multuplicação
+    console.log(3 * 5)
+    // divisão
+    console.log(15 / 3)
+    // soma
+    console.log(20 + 100)
+    // subtração
+    console.log(100 - 20)
+    // resto da divisão
+    let qualquer
+    qualquer = 10 % 3
+    console.log(qualquer)
+    // incremento
+    let num = 0
+    console.log(++num)
+    // decremento
+    console.log(--num)
+    // exponencial
+    console.log(2 **3)
