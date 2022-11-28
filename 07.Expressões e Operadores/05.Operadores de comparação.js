e294cea50098fd23b7d3f9ccf476c5c3d74cb577
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-28 19:47:44 -0300

    Operadores de Comparação

---
+++ 07.Expressões e Operadores/05.Operadores de comparação.js
@@ -0,0 +1,42 @@
+/*
+    Irá comparar valorese retornar um Boolean como 
+    resposta à comparação
+*/ 
+    let one = 1
+    let two = 2
+
+    // == igual a 
+    // console.log(two == 1)
+    // console.log(one == '1')
+
+    // != diferente de 
+    // console.log(two != 1)
+    // console.log(one != 1)
+    // console.log(one != '1')
+
+// ----------------------------------------------------- 
+
+    // === estritamente igual a 
+    // console.log(one === '1')
+    // console.log(one === 1)
+
+    // !== estritamente diferente de 
+    // console.log(two !== '2')
+    // console.log(two !== 2)
+
+// ------------------------------------------------------
+
+    // >         maior que 
+    console.log(one > two)
+
+    // >=        maior igual a  
+    console.log(one >= 1)
+    console.log(two >= 1)
+
+    //  <        menor que 
+    console.log(one < two)
+
+    // <=        menor igual a 
+    console.log(one <= two)
+    console.log(one <= 1)
+    console.log(one <= 0)
