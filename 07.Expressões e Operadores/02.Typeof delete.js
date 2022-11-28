486780989a22e864a43c04e84dc3e2fcf0f41486
Miguel Santos <miguel.santoss085@gmail.com>
2022-11-28 19:47:10 -0300

    Typeof e Delete

---
+++ 07.Expressões e Operadores/02.Typeof delete.js
@@ -0,0 +1,17 @@
+/*
+    Operadores unários
+    typeof
+    delete
+*/ 
+
+console.log(typeof 'miguel')
+
+const person = {
+    name: 'lucas',
+    age: 32
+}
+console.log(person)
+
+delete person.age
+
+console.log(person)
