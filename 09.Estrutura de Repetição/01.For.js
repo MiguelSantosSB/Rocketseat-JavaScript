55d8dc47fdf610875107fd2b4e5cf8f2f93b5239
Miguel Santos <miguel.santoss085@gmail.com>
2022-12-04 14:41:05 -0300

    For

---
+++ 09.Estrutura de Repetição/01.For.js
@@ -0,0 +1,13 @@
+// Estrutura de repetição
+
+for(let i = 0; i < 10; i++){
+    if(i === 5){
+        // break
+        continue
+    }
+    console.log(i)
+}
+
+// break - para a repetição
+// continue - pula arepetição desejada
+
