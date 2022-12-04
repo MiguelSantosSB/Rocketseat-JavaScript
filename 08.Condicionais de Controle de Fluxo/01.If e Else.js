047d3958b24d232794a15d0c9997fb06da7572c5
Miguel Santos <miguel.santoss085@gmail.com>
2022-12-04 14:40:01 -0300

    If e Else

---
+++ 08.Condicionais de Controle de Fluxo/01.If e Else.js
@@ -0,0 +1,11 @@
+let temperatura = 37
+
+if(temperatura >= 37.5){
+    console.log("Febre")
+}
+else if(temperatura >= 37 && temperatura < 37.5){
+    console.log("Febre baixa")
+}
+else{
+    console.log("saudavel")
+}
